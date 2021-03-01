aa5efb9ed41313b80750f1264335c878c3c8fe9b
Paloma Ramírez Meza <p.ramirez.m.8080@gmail.com>
2021-03-01 11:49:21 -0600

    First version

---
+++ main.js
@@ -0,0 +1,5 @@
+$red = document.getElementById('red');
+$blue = document.getElementById('blue');
+$yellow = document.getElementById('yellow');
+$green = document.getElementById('green');
+$btnPlay = document.getElementById('btn-play');
