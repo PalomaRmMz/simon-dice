462e008854f5db821cd1b559db61073073ebb2d3
Paloma Ramírez Meza <p.ramirez.m.8080@gmail.com>
2021-03-01 13:06:32 -0600

    Add first text in led, points and level

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-$red = document.getElementById('red');
-$blue = document.getElementById('blue');
-$yellow = document.getElementById('yellow');
-$green = document.getElementById('green');
-$btnPlay = document.getElementById('btn-play');
+$red = document.getElementById("red");
+$blue = document.getElementById("blue");
+$yellow = document.getElementById("yellow");
+$green = document.getElementById("green");
+$btnPlay = document.getElementById("btn-play");
+
+$ledText = document.getElementById("led-text");
+$ledText.innerHTML = "--------";
+$pointText = document.getElementById("point-text");
+$pointText.innerHTML = "--";
+$levelText = document.getElementById("level-text");
+$levelText.innerHTML = "--";
+
+class Game {
+  constructor() {
+    this.init();
+  }
+  init() {
+    $ledText.innerHTML = "Simón dice...";
+    $pointText.innerHTML = "1 pt";
+    $levelText.innerHTML = "1 lvl";
+  }
+}
+function playGame() {
+  let game = new Game();
+}
